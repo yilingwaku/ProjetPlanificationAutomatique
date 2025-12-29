2691b2849026035921233b22616ec6b073832cfa
yilingwaku <yang@YANG-HONOR.localdomain>
2025-12-29 15:59:53 +0100

    Initial ASP project with PDDL4J and benchmarks

---
+++ src/fr/uga/pddl4j/examples/asp/ASP.java
@@ -0,0 +1,175 @@
+package fr.uga.pddl4j.examples.asp;
+
+import fr.uga.pddl4j.heuristics.state.StateHeuristic;
+import fr.uga.pddl4j.parser.DefaultParsedProblem;
+import fr.uga.pddl4j.parser.RequireKey;
+import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.plan.SequentialPlan;
+import fr.uga.pddl4j.planners.AbstractPlanner;
+import fr.uga.pddl4j.planners.Planner;
+import fr.uga.pddl4j.planners.PlannerConfiguration;
+import fr.uga.pddl4j.planners.ProblemNotSupportedException;
+import fr.uga.pddl4j.planners.SearchStrategy;
+import fr.uga.pddl4j.planners.statespace.search.Node;
+import fr.uga.pddl4j.planners.statespace.search.StateSpaceSearch;
+import fr.uga.pddl4j.problem.DefaultProblem;
+import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.State;
+import fr.uga.pddl4j.problem.operator.Action;
+import fr.uga.pddl4j.problem.operator.ConditionalEffect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * The class is an example. It shows how to create a simple A* search planner able to
+ * solve an ADL problem by choosing the heuristic to used and its weight.
+ *
+ * @author D. Pellier
+ * @version 4.0 - 30.11.2021
+ */
+@CommandLine.Command(name = "ASP",
+        version = "ASP 1.0",
+        description = "Solves a specified planning problem using A* search strategy.",
+        sortOptions = false,
+        mixinStandardHelpOptions = true,
+        headerHeading = "Usage:%n",
+        synopsisHeading = "%n",
+        descriptionHeading = "%nDescription:%n%n",
+        parameterListHeading = "%nParameters:%n",
+        optionListHeading = "%nOptions:%n")
+
+public class ASP extends AbstractPlanner {
+
+    /**
+     * The weight of the heuristic.
+     */
+    private double heuristicWeight;
+
+    /**
+     * The name of the heuristic used by the planner.
+     */
+    private StateHeuristic.Name heuristic;
+
+    /**
+     * Sets the weight of the heuristic.
+     *
+     * @param weight the weight of the heuristic. The weight must be greater than 0.
+     * @throws IllegalArgumentException if the weight is strictly less than 0.
+     */
+    @CommandLine.Option(names = {"-w", "--weight"}, defaultValue = "1.0",
+            paramLabel = "<weight>", description = "Set the weight of the heuristic (preset 1.0).")
+    public void setHeuristicWeight(final double weight) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight <= 0");
+        }
+        this.heuristicWeight = weight;
+    }
+
+    /**
+     * Set the name of heuristic used by the planner to the solve a planning problem.
+     *
+     * @param heuristic the name of the heuristic.
+     */
+    @CommandLine.Option(names = {"-e", "--heuristic"}, defaultValue = "FAST_FORWARD",
+            description = "Set the heuristic : AJUSTED_SUM, AJUSTED_SUM2, AJUSTED_SUM2M, COMBO, "
+                    + "MAX, FAST_FORWARD SET_LEVEL, SUM, SUM_MUTEX (preset: FAST_FORWARD)")
+    public void setHeuristic(StateHeuristic.Name heuristic) {
+        this.heuristic = heuristic;
+    }
+
+    /**
+     * Returns the name of the heuristic used by the planner to solve a planning problem.
+     *
+     * @return the name of the heuristic used by the planner to solve a planning problem.
+     */
+    public final StateHeuristic.Name getHeuristic() {
+        return this.heuristic;
+    }
+
+    /**
+     * Returns the weight of the heuristic.
+     *
+     * @return the weight of the heuristic.
+     */
+    public final double getHeuristicWeight() {
+        return this.heuristicWeight;
+    }
+
+    /**
+     * The class logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(ASP.class.getName());
+
+    /**
+     * Instantiates the planning problem from a parsed problem.
+     *
+     * @param problem the problem to instantiate.
+     * @return the instantiated planning problem or null if the problem cannot be instantiated.
+     */
+    @Override
+    public Problem instantiate(DefaultParsedProblem problem) {
+        final Problem pb = new DefaultProblem(problem);
+        pb.instantiate();
+        return pb;
+    }
+
+    /**
+     * Search a solution plan to a specified domain and problem using A*.
+     *
+     * @param problem the problem to solve.
+     * @return the plan found or null if no plan was found.
+     */
+
+    @Override
+    public Plan solve(final Problem problem) {
+        // Creates the A* search strategy
+        StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.ASTAR,
+                this.getHeuristic(), this.getHeuristicWeight(), this.getTimeout());
+        LOGGER.info("* Starting A* search \n");
+        // Search a solution
+         Plan plan = search.searchPlan(problem);
+
+//        final Node goal = search.searchSolutionNode(problem);
+//        Planner.getLogger().trace(problem.toString(goal));
+//        Plan plan = search.extractPlan(goal, problem);
+
+        // If a plan is found update the statistics of the planner and log search information
+        if (plan != null) {
+            LOGGER.info("* A* search succeeded\n");
+            this.getStatistics().setTimeToSearch(search.getSearchingTime());
+            this.getStatistics().setMemoryUsedToSearch(search.getMemoryUsed());
+        } else {
+            LOGGER.info("* A* search failed\n");
+        }
+        // Return the plan found or null if the search fails.
+        return plan;
+    }
+
+    @Override
+    public boolean isSupported(Problem problem) {
+        return true;
+    }
+
+
+    /**
+     * The main method of the <code>ASP</code> planner.
+     *
+     * @param args the arguments of the command line.
+     */
+    public static void main(String[] args) {
+        try {
+            final ASP planner = new ASP();
+            CommandLine cmd = new CommandLine(planner);
+            cmd.execute(args);
+        } catch (IllegalArgumentException e) {
+            LOGGER.fatal(e.getMessage());
+        }
+    }
+}
